669a94ae062785553de130a46ea9dad50ffb8058
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2021-04-11 16:25:38 +0300

    Basic playlist feature

--- ucr.py
+++ ucr.py
@@ -2,12 +2,22 @@ from mutagen.wave import WAVE
 from mutagen.flac import FLAC
 from mutagen.mp3 import MP3
 import os
+import random
+import sys, select
 
 def create_filler(filler_file, seconds):
     os.system("sox " + filler_file + " filler-trimmed.wav trim 0 0:" + seconds + ".000 fade 0 -0 5 > /dev/null 2>&1")
 
 def play_with_vlc(file):
     os.system("vlc -I dummy " + file + " --play-and-exit > /dev/null 2>&1")
+
+def read_playlist(playlist_file):
+    with open(playlist_file) as f:
+        content = f.read().splitlines()
+    
+    return random.choice(content)
+    
+    
 
 def get_lenght(audio_file):
     audio_length = -1
@@ -35,6 +45,11 @@ def get_artist_title(audio_file):
 
     artist = ""
     title = ""
+    
+    # If filepath has spaces it must be enclosed in quotes to play in vlc
+    # ...but with mutagen it should not be in quotes, so lets remove quotes...
+    if audio_file.lower().endswith('"') and audio_file.lower().startswith('"'):
+        audio_file = audio_file[1:-1]
     try:
         if audio_file.lower().endswith("mp3"):
             audio = MP3(audio_file)
@@ -76,37 +91,49 @@ def get_artist_title(audio_file):
 
 print("This is Underground Campus Radio!")
 
-print("Next program starting soon...")
-create_filler("filler.wav", "25")
-play_with_vlc("filler-trimmed.wav")
+# Custom playlist for testing...
+playlist = "example.playlist"
+if len(sys.argv) == 2 and sys.argv[1].endswith(".playlist"):
+    print("Setting playlist to " + sys.argv[1] + " (hope you know what you are doing...)")
+    playlist = sys.argv[1]
 
-artist_title = get_artist_title("song1.mp3")
-print("Now playing:", artist_title[0], "-", artist_title[1])
-#print("Now playing: Dead Kennedys - Hyperactive Child... (" + str(get_lenght("song1.mp3")) + " seconds)")
-play_with_vlc("song1.mp3")
 
-print("Next program starting soon...")
-create_filler("filler.wav", "15")
-play_with_vlc("filler-trimmed.wav")
+radio_on = True
 
-artist_title = get_artist_title("song2.mp3")
-print("Now playing:", artist_title[0], "-", artist_title[1])
-play_with_vlc("song2.mp3")
+while radio_on:
+    test = random.randint(0,19)
 
-artist_title = get_artist_title("song3.flac")
-print("Now playing:", artist_title[0], "-", artist_title[1])
-play_with_vlc("song3.flac")
+    if test == 4:
+        filler_length = random.randint(15,45)
+        print("Next program starting soon...")
+        create_filler("filler.wav", str(filler_length))
+        play_with_vlc("filler-trimmed.wav")
+    
+    # example.playlist is assuming that the listed files will be at
+    # current users Music directory
+    # Playlist creation
+    # find ~/Music -type f > example.playlist + some editing...
 
-print("Next program starting soon...")
-create_filler("filler.wav", "40")
-play_with_vlc("filler-trimmed.wav")
+    song = '"/home/' + os.getlogin() + read_playlist(playlist) + '"'
+    artist_title = get_artist_title(song)
+    print("Now playing:", artist_title[0], "-", artist_title[1])
+    play_with_vlc(song)
+    print("To stop playing press any key")
+    inputs, outputs, errors = select.select( [sys.stdin], [], [], 2 )
+    for keypress in inputs:
+        if keypress == sys.stdin:
+            input_data = sys.stdin.readline()
+            # print("enteri found!")
+            radio_on = False
+    print()
+    
+    #if inputs:
+    #    print("You said", sys.stdin.readline().strip())
+    #    print("toimii")
+    #x = False
+    
 
-artist_title = get_artist_title("program.mp3")
-print("Now playing:", artist_title[0], "-", artist_title[1])
-play_with_vlc("program.mp3")
 
-artist_title = get_artist_title("filler.wav")
-print("Now playing:", artist_title[0], "-", artist_title[1])
-play_with_vlc("filler.wav")
+# print(read_playlist("example.playlist"))
 
 print("This is Underground Campus Radio - program ending for the day, see you later!")
